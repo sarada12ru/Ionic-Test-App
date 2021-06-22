48017536852b586af71c9794dd5dfb25198228c2
Sarada Prasad <saradaprasad.b@intelgic.com>
2021-06-22 22:32:18 +0530

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'Test-App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
